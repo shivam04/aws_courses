104b3e729b5a093de2a47c8f9fe80497a72d1eee
shivam04 <sinhashivam04@gmail.com>
2024-09-26 20:44:57 +0530

    aspect tag

--- cdk_course/photos-stack/bin/photos-stack.ts
+++ cdk_course/photos-stack/bin/photos-stack.ts
@@ -3,9 +3,13 @@ import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
 import { PhotosStack } from '../lib/photos-stack-stack';
 import { PhotosHandlerStack } from '../lib/PhotosHandlerStack';
+import { BucketTagger } from './Tagger';
 
 const app = new cdk.App();
 const photosStack = new PhotosStack(app, 'PhotosStack');
 new PhotosHandlerStack(app, 'PhotosHandlerStack', {
     targetBuckerArn: photosStack.photosbucketArn
 });
+
+const tagger = new BucketTagger('level', 'test');
+cdk.Aspects.of(app).add(tagger);
