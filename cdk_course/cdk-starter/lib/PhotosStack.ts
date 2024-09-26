c7f81859769527b2a946bd3c7cd3a1a2612cab2c
shivam04 <sinhashivam04@gmail.com>
2024-09-26 19:37:50 +0530

    photosstack

--- cdk_course/cdk-starter/lib/PhotosStack.ts
+++ cdk_course/cdk-starter/lib/PhotosStack.ts
@@ -1,11 +1,18 @@
 import * as cdk from 'aws-cdk-lib';
-import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { Bucket, CfnBucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 
 export class PhotosStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        new Bucket(this, 'PhotosBucket');
+        /**
+         * Create new resource.
+         * delete the old one
+         */
+        const myBucket = new Bucket(this, 'PhotosBucket2', {
+            bucketName: 'photosbucket-ddsf4435'
+        });
+        (myBucket.node.defaultChild as CfnBucket).overrideLogicalId('PhotosBucket2atdrf2564');
     }
 }
