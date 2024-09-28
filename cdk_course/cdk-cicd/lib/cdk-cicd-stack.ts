3ea2e5df046203ddfe20e7d789a7031c30709361
shivam04 <sinhashivam04@gmail.com>
2024-09-28 17:34:46 +0530

    Added test stage and lambda stack

--- cdk_course/cdk-cicd/lib/cdk-cicd-stack.ts
+++ cdk_course/cdk-cicd/lib/cdk-cicd-stack.ts
@@ -1,12 +1,13 @@
 import * as cdk from 'aws-cdk-lib';
 import { CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
 import { Construct } from 'constructs';
+import { PipelineStage } from './PipelineStage';
 
 export class CdkCicdStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    new CodePipeline(this, 'AwesomePipeline', {
+    const pipeline = new CodePipeline(this, 'AwesomePipeline', {
       pipelineName: 'AwesomePipeline',
       synth: new ShellStep('Synth', {
         input: CodePipelineSource.gitHub('shivam04/aws_courses', 'cicd-practice'),
@@ -19,6 +20,10 @@ export class CdkCicdStack extends cdk.Stack {
         primaryOutputDirectory: 'cdk_course/cdk-cicd/cdk.out'
       })
     });
+
+    const testStage = pipeline.addStage(new PipelineStage(this, 'PipelineStage', {
+      stageName: 'test'
+    }));
     
   }
 }
